ff2e5a58da662f2fc4cbe58f14950a831d4a09d9
HOME <trinhtunglam2003@gmail.com>
2025-04-08 16:36:44 +0700

    can update order status

--- src/main/java/com/example/doan/service/OrderService.java
+++ src/main/java/com/example/doan/service/OrderService.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import com.example.doan.dto.OrderDTO;
 import com.example.doan.entity.Orders;
+import com.example.doan.entity.UserEntity;
 
 public interface OrderService {
     public OrderDTO createOrderFromCart(Long userId, Long shippingAddress, String paymentMethod, String shippingMethod);
     List<OrderDTO> getOrderHistoryByUser(Long userId);
      Orders updatePaymentStatus(Long orderId);
+    UserEntity getUserById(Long userId);
+    Orders getOrderById(Long orderId);
+    void saveOrder(Orders order);
 }
