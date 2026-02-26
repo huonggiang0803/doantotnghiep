8946323aeae1662c125d2e32f720f33eb1b061dd
tunglam <trinhtunglam2003@gmail.com>
2026-02-26 09:18:08 +0700

    delete folder fe

--- frontend/client/src/assets/images/index.js
+++
@@ -1,5 +0,0 @@
-const images = {
-  logo: require('~/assets/images/logo.svg').default,
-  logoAdidas: require('~/assets/images/logoAdidas.sgv').default,
-};
-export default images;
--- frontend/client/src/pages/BrandManagement/index.js
+++
@@ -1,1 +0,0 @@
-export { default } from './BrandManagement.js';
